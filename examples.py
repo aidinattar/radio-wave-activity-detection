837e19575c46778809cb5f41bf8feb2c9364b7a2
Aidin Attar <attaraidin@gmail.com>
2023-03-25 12:29:12 +0100

    update examples

--- examples.py
+++ examples.py
@@ -1,5 +1,6 @@
 # %%
 from DataReader import DataReader
+from DataCutter import DataCutter
 
 subjects = [f'subject_{i:02d}' for i in range(5, 6)]
 sets     = [f'set{i:03d}'      for i in range(1, 2)]
@@ -21,5 +22,18 @@ a.plot_mDoppler_map(start=161, stop=208)
 # %%
 #a.plot_mDoppler_map_3d()
 # %%
-a.divide_actions(11)
+a.timestamp_to_bins(11)
+# %%
+a.divide_actions()
+
+# %%
+b = DataCutter(a)
+b.cut(11)
+# %%
+b.create_labels_list()
+# %%
+b.labels_to_int()
+
+# %%
+b.save()
 # %%
