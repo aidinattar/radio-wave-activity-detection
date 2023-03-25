3d292d12116a73f65b744d3f7b80bcdada2fbff9
Aidin Attar <attaraidin@gmail.com>
2023-03-25 12:28:35 +0100

    add main structure

---
+++ train.py
@@ -0,0 +1,14 @@
+'''
+train.py
+
+This file contains the train function, which is used to train the model.
+
+The model is trained using the Adam optimizer and the Cross Entropy loss.
+'''
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
