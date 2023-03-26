5a4a02c3667762b7c45803cf75420d0f90fc3c23
Aidin Attar <attaraidin@gmail.com>
2023-03-26 18:30:07 +0200

    add train-test split function

--- models/classifier.py
+++ models/classifier.py
@@ -1,23 +1,48 @@
 '''
 classifier.py
 '''
-from preprocessing.DataProcess import DataProcess
-from exceptions import OptionIsFalseError
-from sklearn.model_selection import train_test_split
+import numpy as np
+from torch.utils.data          import DataLoader, random_split
+from preprocessing.dataset     import Dataset
+from exceptions                import OptionIsFalseError
+from sklearn.model_selection   import train_test_split
+from cnn_rd                    import CNN_RD
+from cnn_md                    import CNN_MD
 
 class model(object):
     '''
     Class to create the model
     '''
 
-    def __init__(self, data: DataProcess):
+    def __init__(self, data: Dataset, case: int=0, model_type: str='CNN-MD'):
         '''
         Constructor
+
+        Parameters
+        ----------
+        data : Dataset
+            Dataset object.
+        case : int, optional
+            Case to use. The default is 0.
+            Possible values are:
+                0: train and test separately on the radars
+                1: train on one radar and test on the other
+                2: train and test on all the data
+        model_type : str, optional
+            Type of model to create. Possible values are:
+            'CNN-MD', 'CNN-RD'. The default is 'CNN-MD'.
         '''
-        pass
+        # Get the data
+        self.data = data
+
+        # Get the case
+        self.case = case
+
+        # Get the model type
+        self.model_type = model_type
 
 
-    def create_model(self, model_type: str='CNN-MD'):
+    def create_model(self):
         '''
         Create the model
 
@@ -31,7 +56,12 @@ class model(object):
         pass
 
 
-    def train_test_split(self, test_size: float=0.2):
+    def train_test_split(self,
+                         test_size: float=0.2,
+                         random_state: int=42,
+                         batch_size: int=32,
+                         shuffle: bool=True,
+                         num_workers: int=0):
         '''
         Split the data into training and testing data
 
@@ -39,8 +69,38 @@ class model(object):
         ----------
         test_size : float, optional
             Size of the test data. The default is 0.2.
+        random_state : int, optional
+            Random state. The default is 42.
+        batch_size : int, optional
+            Batch size. The default is 32.
+        shuffle : bool, optional
+            Shuffle the data. The default is True.
+        num_workers : int, optional
+            Number of workers. The default is 0.
         '''
-        pass
+        # Split the data
+        if self.case == 0:
+            train_size = int((1-test_size) * len(self.data))
+            test_size = len(self.data) - train_size
+            self.train_data, self.test_data = random_split(self.data, [train_size, test_size])
+        elif self.case == 1:
+            # TODO: split the data into training and testing data
+            pass
+        elif self.case == 2:
+            # TODO: split the data into training and testing data
+            pass
+        else:
+            raise ValueError('Invalid case')
+
+        # Create the data loaders
+        self.train_loader = DataLoader(self.train_data,
+                                        batch_size=batch_size,
+                                        shuffle=shuffle,
+                                        num_workers=num_workers)
+        self.test_loader = DataLoader(self.test_data,
+                                        batch_size=batch_size,
+                                        shuffle=shuffle,
+                                        num_workers=num_workers)
 
 
     def train_model(self):
