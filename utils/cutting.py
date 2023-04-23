a9c7749772d3b71b441792d69f6424369174b44d
Aidin Attar <attaraidin@gmail.com>
2023-04-23 09:51:49 +0200

    correct gaussian extraction method

--- utils/cutting.py
+++ utils/cutting.py
@@ -29,8 +29,25 @@ def random(array: np.ndarray, len_default: int = 40):
         random_bins.sort()
         return array[random_bins]
 
-### Doubt: can we extract indexes according to a normal
-### distribution without replacements?
+
+def gaussian(array: np.ndarray,
+             loc: float=20,
+             scale: float=5):
+    '''
+    Get the probability of a value x according to a gaussian distribution
+    
+    Parameters
+    ----------
+    array : np.ndarray
+        array of values.
+    loc : float, optional
+        Mean of the gaussian distribution. The default is 20.
+    scale : float, optional
+        Standard deviation of the gaussian distribution. The default is 5.
+    '''
+    return np.exp(-np.power(array - loc, 2.) / (2 * np.power(scale, 2.)))
+
+
 def normal(array: np.ndarray,
            len_default:int=40,
            mean:int=20,
@@ -55,14 +72,18 @@ def normal(array: np.ndarray,
     else:
         # select len_default bins according to a normal
         # distribution and take the data sorted by time
-        #### NO np.random.normal
-        #### USE np.random.choice with custom probability
-        #### Assign probabilities accoreding to a normal distribution
-        normal_bins = np.random.normal(
+        normal_distrib = gaussian(
+            array=array,
             loc=mean,
             scale=std,
-            size=len_default
         )
-        normal_bins = np.round(normal_bins).astype(int)
+
+        normal_bins = np.random.choice(
+            a=array.shape[0],
+            size=len_default,
+            replace=False,
+            p=normal_distrib
+        )
+
         normal_bins.sort()
         return array[normal_bins]
