cb5e91bbab7c74bb5b14c2b75074fc09dedc236d
Aidin Attar <attaraidin@gmail.com>
2023-04-23 09:52:03 +0200

    correct main

--- train.py
+++ train.py
@@ -6,17 +6,19 @@ This file contains the train function, which is used to train the model.
 The model is trained using the Adam optimizer and the Cross Entropy loss.
 
 Usage:
-    train.py <model> <data> <input> <case> [--epochs=<epochs>] [--batch_size=<batch_size>] [--lr=<lr>] [--weight_decay=<weight_decay>] [--momentum=<momentum>] [--nesterov=<nesterov>] [--patience=<patience>] [--min_delta=<min_delta>] [--factor=<factor>] [--verbose=<verbose>] [--seed=<seed>]
+    train.py <model> <data> <input> <case> (--load|--no-load) [--epochs=<epochs>] [--batch_size=<batch_size>] [--optimizer=<optimizer>] [--lr=<lr>] [--weight_decay=<weight_decay>] [--momentum=<momentum>] [--nesterov=<nesterov>] [--loss=<loss>] [--patience=<patience>] [--min_delta=<min_delta>] [--factor=<factor>] [--verbose=<verbose>] [--seed=<seed>]
     train.py -h | --help
 
 Options:
     -h --help                       Show this screen.
     --epochs=<epochs>               Number of epochs [default: 100].
     --batch_size=<batch_size>       Batch size [default: 32].
+    --optimizer=<optimizer>         Optimizer [default: Adam].
     --lr=<lr>                       Learning rate [default: 0.001].
     --weight_decay=<weight_decay>   Weight decay [default: 0.0001].
     --momentum=<momentum>           Momentum [default: 0.9].
-    --nesterov=<nesterov>           Nesterov [default: True].
+    --nesterov=<nesterov>           Nesterov [default: False].
+    --loss=<loss>                   Loss [default: CrossEntropyLoss].
     --patience=<patience>           Patience [default: 10].
     --min_delta=<min_delta>         Minimum delta [default: 0.0001].
     --factor=<factor>               Factor [default: 0.1].
@@ -24,7 +26,7 @@ Options:
     --seed=<seed>                   Seed [default: 42].
 
 Example:
-    python train.py CNN-MD data_processed.npz mDoppler 2 --epochs=1500 --batch_size=32 --lr=0.001 --weight_decay=0.0001 --momentum=0.9 --nesterov=True --patience=10 --min_delta=0.0001 --factor=0.1 --verbose=1 --seed=42
+    python train.py CNN-MD data_processed.npz mDoppler 2 --load --epochs=1500 --batch_size=32 --optimizer=Adam --lr=0.001 --weight_decay=0.0001 --momentum=0.9 --nesterov=True --loss=Adam --patience=10 --min_delta=0.0001 --factor=0.1 --verbose=1 --seed=42
 '''
 
 # TODO:
@@ -41,7 +43,7 @@ from datetime import datetime
 
 now = datetime.now().strftime("%Y%m%d")
 
-def main(model_name:str, data:Dataset, case, epochs, batch_size, lr, weight_decay, momentum, nesterov, patience, min_delta, factor, verbose, device):
+def main(model_name:str, data:Dataset, case, load, epochs, batch_size, optimizer, lr, weight_decay, momentum, nesterov, loss, patience, min_delta, factor, verbose, device):
     '''
     Train the model, save the best model and save the training history
 
@@ -51,12 +53,16 @@ def main(model_name:str, data:Dataset, case, epochs, batch_size, lr, weight_deca
         Name of the model
     data : DataProcess
         DataProcess object containing the data
-    output : str
-        Path to the output folder
+    case : int
+        Case to use
+    load : bool
+        Load the model
     epochs : int
         Number of epochs
     batch_size : int
         Batch size
+    optimizer : str
+        Optimizer
     lr : float
         Learning rate
     weight_decay : float
@@ -65,6 +71,8 @@ def main(model_name:str, data:Dataset, case, epochs, batch_size, lr, weight_deca
         Momentum
     nesterov : bool
         Nesterov
+    loss : str
+        Loss function
     patience : int
         Patience
     min_delta : float
@@ -80,18 +88,21 @@ def main(model_name:str, data:Dataset, case, epochs, batch_size, lr, weight_deca
     classifier = model(data=data, case=case, model_type=model_name)
     classifier.create_model()
 
+    if load:
+        classifier.load_model(name='checkpoint', path='checkpoints')
+
     # Split the data into training and validation sets
-    classifier.train_test_split()
+    classifier.train_test_split(test_size=.2, batch_size=batch_size)
     
     # Print the model summary
     classifier.summary(save=True)
 
     # Create the optimizer, loss function
-    classifier.create_optimizer()
-    classifier.create_loss()
+    classifier.create_optimizer(optimizer=optimizer, lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov)
+    classifier.create_loss(loss=loss)
 
     # Train the model
-    classifier.train_model(epochs=epochs)
+    classifier.train_model(epochs=epochs, checkpoint=True)
 
     # Plot the training history
     classifier.plot_history(save=True)
@@ -113,14 +124,18 @@ if __name__ == '__main__':
     # set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    load = bool(args['--load'])
+
     # set hyperparameters
     case = int(args['<case>'])
     epochs = int(args['--epochs'])
     batch_size = int(args['--batch_size'])
+    optimizer = args['--optimizer']
     lr = float(args['--lr'])
     weight_decay = float(args['--weight_decay'])
     momentum = float(args['--momentum'])
     nesterov = bool(args['--nesterov'])
+    loss = args['--loss']
     patience = int(args['--patience'])
     min_delta = float(args['--min_delta'])
     factor = float(args['--factor'])
@@ -137,4 +152,20 @@ if __name__ == '__main__':
     # load model
     model_name = args['<model>']
 
-    main(model_name, data, case, epochs, batch_size, lr, weight_decay, momentum, nesterov, patience, min_delta, factor, verbose, device)
+    main(model_name=model_name,
+         data=data,
+         case=case,
+         load=load,
+         epochs=epochs,
+         batch_size=batch_size,
+         optimizer=optimizer,
+         lr=lr,
+         weight_decay=weight_decay,
+         momentum=momentum,
+         nesterov=nesterov,
+         loss=loss,
+         patience=patience,
+         min_delta=min_delta,
+         factor=factor,
+         verbose=verbose,
+         device=device)
