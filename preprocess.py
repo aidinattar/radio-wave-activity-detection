df99eae898832b4af302b032dd71590766d4a17b
Aidin Attar <attaraidin@gmail.com>
2023-06-08 20:46:42 +0200

    remove gaussian filtering

--- preprocess.py
+++ preprocess.py
@@ -75,12 +75,12 @@ def reading(data_path:str,
     if verbose > 1:
         print("Rescaling...")
     dr.rescaling(method='norm')
-    if verbose > 1:
-        print("Filtering...")
-    if do_mDoppler:
-        if verbose > 1:
-            print("Filtering...")
-        dr.filter_mDoppler(size=(21, 11), sigma=15)
+    #if verbose > 1:
+    #    print("Filtering...")
+    #if do_mDoppler:
+    #    if verbose > 1:
+    #        print("Filtering...")
+    #    dr.filter_mDoppler(size=(21, 11), sigma=15)
     return dr
             
 
